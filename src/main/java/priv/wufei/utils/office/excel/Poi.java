782fdd8aeee01ab7e3a240e3867236aad8c0db50
wufei <wfzlzzhp@163.com>
2018-12-11 02:55:13 +0800

    excel操作添加

--- src/main/java/priv/wufei/utils/office/excel/Poi.java
+++ src/main/java/priv/wufei/utils/office/excel/Poi.java
@@ -1,10 +1,16 @@
 package priv.wufei.utils.office.excel;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import priv.wufei.utils.basis.functions.ConsumerThrow3;
+
+import java.io.File;
+import java.io.IOException;
 
 import static priv.wufei.utils.basis.IOUtils.output;
 
@@ -42,11 +48,9 @@ public final class Poi {
 
         for (int row = 0; row < rows; row++) {
 
-            HSSFRow currentRow = sheet.createRow(row);
-
             for (int col = 0; col < columns; col++) {
                 //得到当前单元格
-                HSSFCell currentCell = currentRow.createCell(col);
+                HSSFCell currentCell = sheet.createRow(row).createCell(col);
                 //向单元格添加数据
                 fillData.accept(currentCell, row, col);
             }
@@ -54,4 +58,111 @@ public final class Poi {
         //写出到磁盘
         output(destFilePath, workbook::write);
     }
+
+    /**
+     * 获得工作薄
+     *
+     * @param srcFilePath 输入文键地址
+     * @return {@link Workbook}
+     * @throws IOException IOException
+     */
+    public static Workbook getWorkbook(String srcFilePath) throws IOException {
+        // 获得工作簿
+        return WorkbookFactory.create(new File(srcFilePath));
+    }
+
+    /**
+     * 获得所有工作表
+     *
+     * @param srcFilePath 输入文键地址
+     * @return {@link Sheet}数组
+     * @throws IOException IOException
+     */
+    public static Sheet[] getSheets(String srcFilePath) throws IOException {
+
+        Workbook workbook = getWorkbook(srcFilePath);
+        // 获得工作表个数
+        int sheetCount = workbook.getNumberOfSheets();
+
+        Sheet[] sheets = new Sheet[sheetCount];
+        // 遍历工作表
+        for (int i = 0; i < sheetCount; i++) {
+
+            sheets[i] = workbook.getSheetAt(i);
+        }
+        return sheets;
+    }
+
+    /**
+     * 获得指定工作表
+     *
+     * @param srcFilePath 输入文键地址
+     * @param sheetName   工作表名
+     * @return {@link Sheet}
+     * @throws IOException IOException
+     */
+    public static Sheet getSheet(String srcFilePath, String sheetName) throws IOException {
+
+        Workbook workbook = getWorkbook(srcFilePath);
+
+        return workbook.getSheet(sheetName);
+    }
+
+    /**
+     * 获得指定工作表的总行数
+     *
+     * @param sheet 工作表
+     * @return int
+     */
+    public static int getRows(Sheet sheet) {
+
+        return sheet.getPhysicalNumberOfRows();
+    }
+
+    /**
+     * 获得指定行的列数
+     *
+     * @param sheet 工作表
+     * @param row   行号
+     * @return int
+     */
+    public static int getColumns(Sheet sheet, int row) {
+
+        return sheet.getRow(row).getPhysicalNumberOfCells();
+    }
+
+    /**
+     * 获得指定工作表的最长一列的列数
+     *
+     * @param sheet 工作表
+     * @return int
+     */
+    public static int getLongestColumns(Sheet sheet) {
+
+        int columns = 0;
+
+        int lastRowNum = sheet.getLastRowNum();
+
+        for (int i = 0; i <= lastRowNum; i++) {
+            //每行的有效单元格
+            int cells = getColumns(sheet, i);
+
+            columns = columns > cells ? columns : cells;
+        }
+        return columns;
+    }
+
+    /**
+     * 获得指定工作表指定行列的单元格
+     *
+     * @param sheet 工作表
+     * @param row   行号
+     * @param col   列号
+     * @return {@link Cell}
+     */
+    public static Cell getCell(Sheet sheet, int row, int col) {
+
+        return sheet.getRow(row).getCell(col);
+    }
+
 }
--- src/test/java/Debug.java
+++ src/test/java/Debug.java
@@ -1,13 +1,6 @@
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.jupiter.api.Test;
 import priv.wufei.utils.basis.functions.RunnerThrow;
-
-import java.io.File;
-import java.io.IOException;
 
 import static priv.wufei.utils.basis.DateTimeUtils.nanoTimeTimekeeping;
 
@@ -33,57 +26,11 @@ public class Debug {
     void test() {
 
         test(() -> {
-            Sheet[] sheets = getSheets(desktop + "xx.xls");
 
-            Sheet sheet = sheets[0];
-            // 获得行数
-            int rows = sheet.getLastRowNum() + 1;
-            // 获得列数，先获得一行，在得到改行列数
-            Row tmp = sheet.getRow(0);
-            int cols = tmp.getLastCellNum();
-            // 读取数据
-            for (int row = 0; row < rows; row++) {
-                Row r = sheet.getRow(row);
-                for (int col = 0; col < cols; col++) {
-                    System.out.printf("%10s", r.getCell(col).getStringCellValue());
-                }
-                System.out.println();
-            }
         });
     }
 
-    public static Workbook getWorkbook(String srcFilePath) throws IOException {
-        // 获得工作簿
-        return WorkbookFactory.create(new File(srcFilePath));
-    }
 
-    public static Sheet[] getSheets(String srcFilePath) throws IOException {
-
-        Workbook workbook = getWorkbook(srcFilePath);
-        // 获得工作表个数
-        int sheetCount = workbook.getNumberOfSheets();
-
-        Sheet[] sheets = new Sheet[sheetCount];
-        // 遍历工作表
-        for (int i = 0; i < sheetCount; i++) {
-
-            sheets[i] = workbook.getSheetAt(i);
-        }
-        return sheets;
-    }
-
-    public static Sheet getSheet(String srcFilePath, String sheetName) throws IOException {
-
-        Workbook workbook = getWorkbook(srcFilePath);
-
-        return workbook.getSheet(sheetName);
-    }
-
-
-
-    /*
-
-     */
 }
 
 
